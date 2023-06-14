04e1eb7349c554d117159f84a327c62464f42f97
idamara moraes <igormat4us@gmail.com>
2023-06-14 02:04:06 -0300

    Fixing problems

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,20 +1,18 @@
 const mongoose = require('mongoose');
-const URL = 'mongodb://0.0.0.0:27017/';
+const URL = 'mongodb://0.0.0.0:27017/catalogo';
+const db = mongoose.connect(URL);
+const con = mongoose.connection;
 
-mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-
-db.on('error', function () {
-  console.log('Erro na conexão com o MongoDB!');
-});
-
-db.once('open', function () {
+con.on('open', function () {
   console.log('Conectado ao MongoDB!');
 });
 
-db.on('close', function () {
-  console.log('Desconectado do MongoDB!');
+con.on('error', function () {
+  console.log('Erro na conexão com o MongoDB!');
+});
+
+con.on('close', function () {
+  console.log('Desconetado do MongoDB!');
 });
 
 module.exports = db;
