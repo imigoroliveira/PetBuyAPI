6d4100452a777029cf0608fd0d1cf31fa81c5c5c
imigoroliveira <igormat4us@gmail.com>
2023-06-20 00:09:51 -0300

    Fixing

--- routes/ProductRouter.js
+++ routes/ProductRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const productController = require('../controllers/productController');
+const productController = require('../controllers/ProductController');
 
 
 const storage = multer.diskStorage({
