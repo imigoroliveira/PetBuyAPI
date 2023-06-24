c50ad5919227c9b30fdcd5acfabc0494b2fee59e
imigoroliveira <igormat4us@gmail.com>
2023-06-24 00:59:37 -0300

    Fixing references problems and additing new endpoint to swagger documetation

--- routes/OrderRouter.js
+++ routes/OrderRouter.js
@@ -4,7 +4,7 @@ const orderController = require('../controllers/OrderController');
 const auth = require('../auth/auth');
 
 router.use(auth.autorizar);
-router.post('/order', orderController.efetuarOrder);
+router.post('/order/create', orderController.efetuarOrder);
 router.put('/order/:codigo/status', orderController.editarStatusOrder);
 router.get('/order/cliente/:clienteId', orderController.retornarOrderPorCliente);
 router.get('/order', orderController.retornarListaOrders);
